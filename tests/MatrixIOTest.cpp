cb495eef8f44d8123eab36be9e01b4f1da64a47f
Lion Wagner <st148345@stud.uni-stuttgart.de>
2023-02-02 19:09:09 +0100

    Fix test runs

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,37 +1,39 @@
 #define BOOST_TEST_DYN_LINK
-
 #include <Eigen/Dense>
 #include <boost/test/unit_test.hpp>
 #include "Configuration.hpp"
 #include "MatrixIO.hpp"
 
+#include <fstream>
+
 using namespace Eigen;
 
 BOOST_AUTO_TEST_SUITE(MatrixIOTests)
+
+BOOST_AUTO_TEST_CASE(TestSaveData)
 {
-  BOOST_AUTO_TEST_CASE(TestSaveData)
-  {
-    MatrixXd matrix(3, 3);
-    matrix << 1, 2, 3,
-        4, 5, 6,
-        7, 8, 9;
-    matrixIO::saveData("test.csv", matrix);
+  MatrixXd matrix(3, 3);
+  matrix << 1, 2, 3,
+      4, 5, 6,
+      7, 8, 9;
+  matrixIO::saveData("test.csv", matrix);
 
-    struct stat buffer;
-    bool        file_exists = (stat(name.c_str(), &buffer) == 0);
+  std::ifstream f("test.csv");
+  bool        file_exists = f.good();
 
-    BOOST_TEST(file_exists);
-  }
-
-  BOOST_AUTO_TEST_CASE(TestOpenData)
-  {
-    MatrixXd matrix(3, 3);
-    matrix << 1, 2, 3,
-        4, 5, 6,
-        7, 8, 9;
-    matrixIO::saveData("test.csv", matrix);
-
-    MatrixXd matrix2 = matrixIO::openData("test.csv", 3);
-    BOOST_TEST(matrix == matrix2);
-  }
+  BOOST_TEST(file_exists);
 }
+
+BOOST_AUTO_TEST_CASE(TestOpenData)
+{
+  MatrixXd matrix(3, 3);
+  matrix << 1, 2, 3,
+      4, 5, 6,
+      7, 8, 9;
+  matrixIO::saveData("test.csv", matrix);
+
+  MatrixXd matrix2 = matrixIO::openData("test.csv", 3);
+  BOOST_TEST(matrix == matrix2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
